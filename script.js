1f722bc9a85f315b7ec48b74191fc6566769976c
Deepak karki <ideepakkarki@gmail.com>
2025-01-05 15:21:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const submitBtn = document.getElementById("submit");
-  const checkBox = document.querySelector("#checkbox");
-  const userName = document.querySelector("#username");
-  const passWord = document.querySelector("#password");
-  const inputBox = document.querySelector(".inputFields");
-  const form = document.querySelector("form");
+let submitBtn = document.querySelector('input[type="submit"]');
+let checkBox = document.querySelector("#checkbox");
+let userName = document.querySelector("#username");
+let passWord = document.querySelector("#password");
+let inputBox = document.querySelector(".inputFields");
+let form = document.querySelector("form");
 
-  const savedData = JSON.parse(localStorage.getItem("userdata"));
+let savedData = JSON.parse(localStorage.getItem("userdata"));
 
-  if (savedData) {
-    const existingBtn = document.createElement("button");
+function storeData() {
+  let userData = {};
+
+  if (localStorage.length > 0) {
+    let existingBtn = document.createElement("button");
     existingBtn.id = "existing";
     existingBtn.innerHTML = "Login as existing user";
     form.append(existingBtn);
@@ -19,20 +21,22 @@ document.addEventListener("DOMContentLoaded", () => {
       e.preventDefault();
       alert(`Logged in as ${savedData.username}`);
     });
+  } else if (localStorage.length < 0) {
+    inputBox.style.display = "block !important";
+    form.remove(existingBtn);
   }
 
   submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
-
     if (checkBox.checked) {
-      const userData = {
-        username: userName.value,
-        password: passWord.value,
-      };
+      userData.username = userName.value;
+      userData.password = passWord.value;
       localStorage.setItem("userdata", JSON.stringify(userData));
       alert(`Logged in as ${userData.username}`);
-    } else {
+    } else if (!checkBox.checked) {
       localStorage.removeItem("userdata");
     }
   });
-});
+}
+
+storeData();
